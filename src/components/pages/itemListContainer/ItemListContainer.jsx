300bfcc8e0f9c65a3ac0c6e354c278561655a635
diego <d.e.fermoselle@gmail.com>
2024-10-11 02:40:20 -0300

    fin entrega 1

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,8 +1,9 @@
 // import { Card } from "../../common/Card/Card";
 
-export const ItemListContainer = () => {
+export const ItemListContainer = ({ greetings }) => {
   return (
     <div>
+      <h2>{greetings} </h2>
       {/* <Card ATRIBUTO y VALOR -  le paso los props que querramos/>  */}
       {/* <Card title="Maceta" price={2000} stock={50} /> */}
     </div>
