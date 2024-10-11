2b4becaf3ae827392fcc25ea837e0dd7016ba610
diego <d.e.fermoselle@gmail.com>
2024-10-11 00:29:09 -0300

    limpieza nav

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -4,12 +4,16 @@ export const Navbar = () => {
   return (
     <div className="container-navbar">
       <h1>3DCOMMERSE</h1>
-      <li>Decoracíon</li>
-      <li>Arte</li>
-      <li>Moda</li>
-      <li>Joyas</li>
-      <li>Casa</li>
-      <li>Arquitectura</li>
+
+      <ul>
+        <li>Decoracíon</li>
+        <li>Arte</li>
+        <li>Moda</li>
+        <li>Joyas</li>
+        <li>Casa</li>
+        <li>Arquitectura</li>
+      </ul>
+      <h2>Carrito</h2>
     </div>
   );
 };
